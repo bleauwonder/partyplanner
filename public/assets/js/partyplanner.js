d248e3f1b26add57b336c21adeee1821ffc605e7
bleauwonder <elyzableau.om@gmail.com>
2019-07-26 22:41:01 -0400

    add task css and more color changes

--- public/assets/js/partyplanner.js
+++ public/assets/js/partyplanner.js
@@ -60,3 +60,20 @@ $(function() {
     });
   });
   
+  const buttons = document.querySelectorAll('.button');
+  buttons.forEach((button) => {
+    let target = button.querySelector('.target');
+    function handleMove(e) {
+      const x = -50 + (e.pageX - button.offsetLeft - 300 / 2) / 3;
+      const y = -10 + (e.pageY - button.offsetTop - 100 / 2) / 3;
+  
+      target.style.setProperty('--x', `${ x }px`)
+      target.style.setProperty('--y', `${ y }px`)
+    }
+    button.addEventListener('mousemove', (e) => {
+      handleMove(e);
+    });
+    button.addEventListener('touchmove', (e) => {
+      handleMove(e.changedTouches[0]);
+    });
+  });
